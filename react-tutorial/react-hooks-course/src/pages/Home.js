a83967e96ca118ba0a0649c50d0d78847ecf5643
Vignesh Bharathi K <vigneshbharathi.k@GAVSTECH.COM>
2022-09-19 17:39:45 +0530

    Added small styling in main card component

--- react-tutorial/react-hooks-course/src/pages/Home.js
+++ react-tutorial/react-hooks-course/src/pages/Home.js
@@ -1,9 +1,28 @@
 
+const pages = [
+    { title: "Use State" },
+    { title: "Use Reduce" },
+    { title: "Use Effect" },
+    { title: "Use Ref" },
+    { title: "Use Memo" },
+    { title: "Use Context" },
+    { title: "Use Imperative Handler" },
+    {title: "Use Layout Effect"},
+    { title: "Use Callback" }
+]
 
 export const Home = () => {
     return ( 
         <div className="card-layout">
-            { [1,2,3].map(() => <div className="card-container">This is title</div>) }
+            { pages.map((el) => {
+                return (
+                    <div className="card-container">
+                        <h1 className="card title">
+                        {el.title}
+                        </h1>
+                    </div>
+                )
+            } ) }
         </div>
     )
 }
