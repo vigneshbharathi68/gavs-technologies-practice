3cb3522b65722c32fd1b5ab7d5e03e22c6329b7b
Vignesh Bharathi K <vigneshbharathi.k@gavstech.com>
2022-10-27 16:23:49 +0530

    Angular tutorial practised file added

--- angular-tutorial/angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-tutorial/angular-crash/src/app/components/tasks/tasks.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 
-
-
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -12,11 +10,30 @@ import { Task } from '../../Task';
 export class TasksComponent implements OnInit {
   tasks: Task[] = [];
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.taskService.getTasks();
-    console.log(this.tasks)
+    this.taskService.getTasks().subscribe((task) => this.tasks = task);
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+          () => { 
+            this.tasks = this.tasks.filter(t => t.id !== task.id)
+          }
+        );
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    console.log(task.reminder)
+    this.taskService.updateTaskReminder(task).subscribe()
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task))
   }
 
 }
