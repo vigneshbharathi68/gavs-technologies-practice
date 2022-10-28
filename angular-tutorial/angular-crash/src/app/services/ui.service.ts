b8eeecead4cd4402bbf235d268742ff56d3cfe0b
Vignesh Bharathi K <vigneshbharathi.k@gavstech.com>
2022-10-28 12:09:29 +0530

    Angular task tracker app finished

---
+++ angular-tutorial/angular-crash/src/app/services/ui.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UiService {
+  private showAddTask: boolean = false;
+  private subject = new Subject<any>();
+  constructor() { }
+}
